7980dac6ee00c975656719e0a066352086caff2d
Anna Yureva <yuanya@yandex.ru>
2023-06-07 22:10:32 +0300

    basename remove

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,9 +7,11 @@ import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-      <BrowserRouter basename='/react-mesto-auth'>
-          <App />
-      </BrowserRouter>
+  <React.StrictMode>
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>
+</React.StrictMode>
 );
 
 
